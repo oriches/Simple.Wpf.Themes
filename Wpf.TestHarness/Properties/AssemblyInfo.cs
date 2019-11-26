e3cda2c5dd4af3c487f6f1fda8464062a2d90ae6
Ollie Riches <ollie_riches@hotmail.com>
2019-11-26 21:36:57 +0000

    Updated sln file version info

--- Wpf.TestHarness/Properties/AssemblyInfo.cs
+++ Wpf.TestHarness/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Wpf.TestHarness")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
